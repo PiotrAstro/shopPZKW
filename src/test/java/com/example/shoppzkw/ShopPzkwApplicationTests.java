4354c30a174601eb22bece199fdbf1019b44f424
Piotr Zatwarnicki <piotr.l.zatwarnicki@gmail.com>
2024-12-03 14:18:46 +0100

    initial commiy

---
+++ src/test/java/com/example/shoppzkw/ShopPzkwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppzkw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPzkwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
