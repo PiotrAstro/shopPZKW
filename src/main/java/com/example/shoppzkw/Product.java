0fcab8afaca8b2f8e4ac2109617865dc59eb5796
Piotr Zatwarnicki <piotr.l.zatwarnicki@gmail.com>
2024-12-17 14:48:38 +0100

    added maven dependencies

--- src/main/java/com/example/shoppzkw/Product.java
+++ src/main/java/com/example/shoppzkw/Product.java
@@ -1,73 +1,24 @@
 package com.example.shoppzkw;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Objects;
 
-public class Product {
-    private static int idCounter = 1;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String category;
     private double price;
     private double weight;
-
-    public Product() {
-        this.id = idCounter;
-        idCounter++;
-    }
-
-    public Product(String name, String category, double price, double weight) {
-        this();
-        this.name = name;
-        this.price = price;
-        this.weight = weight;
-        this.category = category;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return id == product.id;
-    }
 }
